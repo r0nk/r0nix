c10953c1aabb855baa782b9072fc5e8d160dd9d3
r0nk <r00nk@simplecpu.com>
2015-03-07 09:55:32 +0000

    k, got as far as possible without mm, now need mm.

--- fs/ext2/find.c
+++ fs/ext2/find.c
@@ -1,30 +1,45 @@
 #include "ext2.h"
 
-/* TODO next_inode(){ */
-
 /*move path to next char, returning the characters moved over*/
-//static char * till_next_slash(char ** path){
-
-//}
+static char * till_next_slash(char ** path){
+	char *r = *path;
+	while(**path!='/' && **path!='\0')
+		(*path)++;
+	**path='\0';
+	(*path)++;
+	return r;
+}
 
 /* return inode if its path is in dir */
-//struct ext2_inode inode_in_dir(struct ext2_dir_entry_2 dir,char * path);
+/*
+struct ext2_inode inode_in_dir(struct ext2_dir_entry_2 dir,char * name){
+	int i;
+	struct ext2_inode inode;
+	for(i=0;i<num_entries;i++)
+		if(ext2_inode)
+			return inode;
+	return NULL;
+}
+*/
+
 //struct ext2_dir_entry_2 dir_by_inode(struct ext2_inode inode);
 
-//struct inode inode_by_path(char * path){
-//	struct ext2_inode inode=ext2_get_root_inode(0);
-//	struct ext2_dir_entry_2 dir;
-//	char * next;
-//	while(path){
-//		next = till_next_slash(&path);
-//		if(!next)/* if no more places to look, then we found it */
-//			return inode;
-//		inode = inode_in_dir(dir,next);
-//		if(!inode)
-//			return NULL
-//		dir = dir_by_inode(inode);
-//		if(!dir)
-//			return NULL;
-//	}
-//	return NULL;
-//}
+/*
+struct inode inode_by_path(char * path){
+	struct ext2_inode inode=ext2_get_root_inode(0);
+	struct ext2_dir_entry_2 dir=dir_by_inode(inode);
+	char * next;
+loop:
+	next = till_next_slash(&path);
+	if(!next)// if no more places to look, then we found it
+		return inode;
+	inode = inode_in_dir(dir,next);
+	if(!inode)
+		return NULL;
+	dir = dir_by_inode(inode);
+	if(!dir)
+		return NULL;
+	goto loop;
+	return NULL;
+}
+*/
--- fs/fdt.c
+++
@@ -1,1 +0,0 @@
-/*File descriptor table*/
