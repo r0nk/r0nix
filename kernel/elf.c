ddaef7295252f49584ef3e9e5655ba005af16cfa
r0nk <r00nk@simplecpu.com>
2015-03-13 21:26:16 +0000

    figuring out how I'm going to load ELF

---
+++ kernel/elf.c
@@ -0,0 +1,5 @@
+#include <elf.h>
+
+//elf_hdr get_elf_hdr(int fd){}
+
+
---
+++ kernel/exec.c
@@ -0,0 +1,16 @@
+/* just some psuedo-code for now */
+
+//void exec(char * program)
+//{
+//	get a program space ready for the program
+//	begin execution of the program.
+//}
+
+//void exec_init_shell()
+//{
+//	char * program;
+//	int fd  = open("/bin/r0sh");
+//	elf_hdr = get_elf_header(fd);
+//	program =(read the rest, based on the header)();
+//	exec(program);
+//}
