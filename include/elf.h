9fc387ee12e34cb4d54e3547c85588c6e878b43a
r0nk <r00nk@simplecpu.com>
2015-03-14 22:06:54 +0000

    added some more to elf.

--- include/elf.h
+++ include/elf.h
@@ -156,14 +156,14 @@ typedef struct elf32_sym{
 
 typedef struct elf32_hdr{
   unsigned char	e_ident[EI_NIDENT];
-  Elf32_Half	e_type;
+  Elf32_Half	e_type;//4*13+16
   Elf32_Half	e_machine;
   Elf32_Word	e_version;
   Elf32_Addr	e_entry;  /* Entry point */
   Elf32_Off	e_phoff;
   Elf32_Off	e_shoff;
   Elf32_Word	e_flags;
-  Elf32_Half	e_ehsize;
+  Elf32_Half	e_ehsize; /* Total size of the header */
   Elf32_Half	e_phentsize;
   Elf32_Half	e_phnum;
   Elf32_Half	e_shentsize;
--- kernel/elf.c
+++ kernel/elf.c
@@ -1,5 +1,10 @@
 #include <elf.h>
+#include <fs.h>
 
-//elf_hdr get_elf_hdr(int fd){}
-
-
+struct elf32_hdr get_elf_hdr(int fd)
+{
+	struct elf32_hdr hdr;
+	char * p =(void *) &hdr;
+	read(fd,p,sizeof(hdr));
+	return hdr;
+}
