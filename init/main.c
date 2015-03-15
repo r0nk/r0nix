f222e0686223d536562b4e33b22e188583936ea8
r0nk <r00nk@simplecpu.com>
2015-03-15 20:43:24 +0000

    getting ready for a userspace.

--- init/main.c
+++ init/main.c
@@ -71,6 +71,6 @@ void initalize_kernel(void * heap,void * multiboot_information)
 void kernel_main(void * heap,void * multiboot_information)
 {
 	initalize_kernel(heap,multiboot_information);
-	exec("/bin/sh");
+	exec("/bin/hello");
 	panic("reached end of kernel_main");
 }
--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -9,17 +9,18 @@ void system_call(struct Cpu_state *s)
 	switch(s->eax){
 		case SYSCALL_OPEN:
 			s->eax = open((void *)s->ebx);
-			return;
+			break;
 		case SYSCALL_READ:
 			s->eax = read(s->ebx,(void *)s->ecx,s->edi);
-			return;
+			break;
 		case SYSCALL_WRITE:
 			s->eax = write(s->ebx,(void *)s->ecx,s->edi);
-			return;
+			break;
 		case SYSCALL_CLOSE:
 		case SYSCALL_FORK:
 		case SYSCALL_EXEC:
-			panic("nyi syscalled \n");
+			panic("nyi syscalled");
 			break;
 	}
+	panic("Syscalled");
 }
