b913806ad75839b4e3283ff51e8376e1be9c3323
r0nk <r00nk@simplecpu.com>
2015-03-27 07:37:04 +0000

    fixed some qemu net issues.

--- scripts/vmrun.sh
+++ scripts/vmrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 qemu-system-i386 -monitor stdio -kernel r0nix -no-reboot -initrd misc/initrd \
- -netdev user,id=network0 -device rtl8139,netdev=network0 -redir tcp:5555::80 \
- -no-kvm-irqchip -d guest_errors
+ -netdev bridge,br=br0,id=network0 -device rtl8139,netdev=network0 \
+ -redir tcp:5555::80 -no-kvm-irqchip -d guest_errors
