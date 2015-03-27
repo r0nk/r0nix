cdb3bb03a05989123d8674fdbc5f51a8a8cc1402
r0nk <r00nk@simplecpu.com>
2015-03-27 02:50:31 +0000

    forget bitswapping,

--- net/arp.c
+++ net/arp.c
@@ -2,14 +2,17 @@
 #include "mac.h"
 #include "arp.h"
 
+
+/*__builtin_bswap */
 void send_arp_request()
 {
 	struct arp_packet apkt;
-	apkt.hrd = ARP_HRD_ETHERNET;
-	apkt.pro = ETHRTYPE_IPV4;
+	apkt.hrd = __builtin_bswap16(ARP_HRD_ETHERNET);
+	apkt.pro = __builtin_bswap16(ETHRTYPE_IPV4);
 	apkt.hln = 6;
 	apkt.pln = 4;
-	apkt.op  = ARP_OP_REQUEST;
+	apkt.op  = __builtin_bswap16(ARP_OP_REQUEST);
+	/* TODO the rest of these still need to be swapped */
 	apkt.sha = our_mac;
 	apkt.spa = 0xabcdefab;/*constant because we dont have proto yet*/
 	apkt.tpa = 0x08080808;/*yay google!*/
