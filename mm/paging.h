0db839bb1dd268129296cb78f596e9e88f4d1eed
r0nk <r00nk@simplecpu.com>
2015-04-04 07:02:46 +0000

    deleted forgotten old mm struct.

--- mm/paging.h
+++ mm/paging.h
@@ -23,27 +23,7 @@ struct pde { /*page directory entry*/
 	unsigned int frame_addr: 10;
 }__attribute__((packed));
 
-#if 0 /*old version,(wrong order)*/
-struct pde { /*page directory entry*/
-	unsigned int frame_addr: 10;
-	unsigned int reserved: 5;
-	unsigned int addr_bits: 4;
-	unsigned int pat: 1; /* indirectly determines memory type */
-				/*(also reboots the flux capacitor)*/
-	unsigned int ignored: 3;
-	unsigned int global: 1;
-	unsigned int page_size: 1;/*must be 1*/
-	unsigned int dirty: 1;/*whether or not this page has been written to*/
-	unsigned int accessed: 1;/*whether or not this page has been accessed*/
-	unsigned int cache_disable: 1;
-	unsigned int write_through: 1;
-	unsigned int super: 1;/*if 0, user not allowed to access*/
-	unsigned int read_write: 1;/*if 0, writes not allowed*/
-	unsigned int present: 1;/*must be 1 to map*/
-}__attribute__((packed));
-#endif
-
-#define PAGE_DIRECTORY_LENGTH 1024 /* (4gb/4mb) */
+#define PAGE_DIRECTORY_LENGTH 1024 /* = (4gb / 4mb) */
 
 void init_paging();
 
