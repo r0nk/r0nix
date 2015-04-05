f215946f17e92729fe63019c83b72230c5ab8720
r0nk <r00nk@simplecpu.com>
2015-04-05 09:18:57 +0000

    more work on the scheduler.

--- kernel/exec.c
+++ kernel/exec.c
@@ -4,28 +4,40 @@
 #include <panic.h>
 #include <time_stamp.h>
 
+#include "process.h"
 #include "elf/elf.h"
 
-void exec(char * path)
+/* parse the executable file and store it into the returned process */
+struct process file_to_process(char * path)
 {
 	unsigned int read_bytes = 0;
 	int fd  = open(path);
 	if(fd<1)
-		panic("exec:fd<1");
+		panic("file_to_process: fd<1");
 	struct elf32_hdr hdr = get_elf_hdr(fd);
 	struct elf32_phdr phdr = get_elf_phdr(fd);
 
 	if(hdr.e_phnum>1)
-		panic("exec: multi-segment program header,nyi");
+		panic("file_to_process: multi-segment program header,nyi");
 
 	/* set the fd read head to zero */
 	close(fd);
 	fd = open(path);
 
 	/* TODO read in the whole file to its given page */
-	panic("exec: read destination nyi");
+	panic("file_to_process: read destination nyi");
 	if(read_bytes!=phdr.p_memsz)
-		panic("exec: not enough bytes read from file");
-	
-	/* TODO add it to the schedular and stuff */
+		panic("file_to_process: not enough bytes read from file");
+}
+
+/* This creates the inital process (which always has pid 1) */
+void exec_inital(char * path)
+{
+	struct process p1 = file_to_process(path);
+	add_process(p1);
+}
+
+void exec(char * path)
+{
+	replace_current_process(file_to_process(path));
 }
--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -1,25 +1,27 @@
 #include <panic.h>
+
 #include "scheduler.h"
 #include "process.h"
 
 /* TODO this should be an array of pointers to processes */
-struct process sproct[MAX_PROCESSES];/* Scheduler PROCess Table */
+struct process sched_procs[MAX_PROCESSES];/* all the current processes */
 
+/* current_process is an index into the sched_procs array, rather then a PID */
 int current_process;
 int total_processes;
 
-/* next_process returns the sproct index for the next process to be run */
+/* next_process returns the sched_procs index for the next process to be run */
 int next_process()
 {
-	/* (its just round robbin for now) */
 	if(current_process >= total_processes)
 		return 1;
+	/* (its just round robbin for now) */
 	return current_process+1;
 }
 
-void task_switch(int pid)
+void task_switch(int index)
 {
-	current_process = pid;
+	current_process = index;
 	/*TODO*/
 	panic("task_switch() nyi");
 }
@@ -32,10 +34,17 @@ void sched_tick()
 	task_switch(next_process());
 }
 
-void add_process()
+void replace_current_process(struct process replacer)
 {
-	/*TODO*/
-	panic("add_process() nyi");
+	sched_procs[current_process]=replacer;
+}
+
+void add_process(struct process p)
+{
+	total_processes++;
+	if(total_processes>MAX_PROCESSES)
+		panic("total processes > max");
+	sched_procs[total_processes]=p;
 }
 
 void init_scheduler()
