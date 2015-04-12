e0e38f90611c57f6844936ac43abccf751744d54
r0nk <r00nk@simplecpu.com>
2015-04-12 10:07:52 +0000

    declared the rest of the syscalls for newlib

--- include/syscalls.h
+++ include/syscalls.h
@@ -2,13 +2,25 @@
 #define SYSCALLS
 #include <cpu.h>
 
-#define SYSCALL_OPEN  0 
-#define SYSCALL_CLOSE 1 
-#define SYSCALL_READ  2
-#define SYSCALL_WRITE 3 
-#define SYSCALL_FORK  4 
-#define SYSCALL_EXEC  5 
-#define SYSCALL_REBOOT 0x99
+#define SYSCALL_OPEN	0 
+#define SYSCALL_CLOSE	1 
+#define SYSCALL_READ	2
+#define SYSCALL_WRITE	3 
+#define SYSCALL_FORK	4 
+#define SYSCALL_EXEC	5 
+#define SYSCALL_EXIT	6 
+#define SYSCALL_FSTAT	7 
+#define SYSCALL_GETPID	8 
+#define SYSCALL_ISATTY	10 
+#define SYSCALL_KILL	11 
+#define SYSCALL_LINK	12 
+#define SYSCALL_LSEEK	13 
+#define SYSCALL_SBRK	14 
+#define SYSCALL_STAT	15 
+#define SYSCALL_TIMES	16 
+#define SYSCALL_UNLINK	17 
+#define SYSCALL_WAIT	18 
+#define SYSCALL_REBOOT	0x99
 
 void system_call(struct cpu_state * s);
 
--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -33,5 +33,45 @@ void system_call(struct cpu_state *s)
 			load_idt(0);
 			asm("int3");
 			break;
+		case SYSCALL_EXIT:
+			panic("SYSCALL_EXIT NYI");
+			break;
+		case SYSCALL_FSTAT:
+			panic("SYSCALL_FSTAT NYI");
+			break;
+		case SYSCALL_GETPID:
+			panic("SYSCALL_GETPID NYI");
+			break;
+		case SYSCALL_ISATTY:
+			panic("SYSCALL_ISATTY NYI");
+			break;
+		case SYSCALL_KILL:
+			panic("SYSCALL_KILL NYI");
+			break;
+		case SYSCALL_LINK:
+			panic("SYSCALL_LINK NYI");
+			break;
+		case SYSCALL_LSEEK:
+			panic("SYSCALL_LSEEK NYI");
+			break;
+		case SYSCALL_SBRK:
+			panic("SYSCALL_SBRK NYI");
+			break;
+		case SYSCALL_STAT:
+			panic("SYSCALL_STAT NYI");
+			break;
+		case SYSCALL_TIMES:
+			panic("SYSCALL_TIMES NYI");
+			break;
+		case SYSCALL_UNLINK:
+			panic("SYSCALL_UNLINK NYI");
+			break;
+		case SYSCALL_WAIT:
+			panic("SYSCALL_WAIT NYI");
+			break;
+		default:
+			kprintf("\ns->eax:%i\n",s->eax);
+			panic("Unknown system call called.");
+			break;
 	}
 }
